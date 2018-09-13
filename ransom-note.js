f8481b7da8755809c619026a28050a929ae7ae7a
Louis Cospain <elcoosp@gmail.com>
2018-09-13 22:16:15 +0200

    feat: 🎸 ransom-note

--- ransom-note.js
+++ ransom-note.js
@@ -1,20 +1,11 @@
-// Return the index of the element or false if not found
-const has = (el, from = 0) => arr => {
-	const i = arr.indexOf(el, from)
-	return i !== -1 ? i : false
-}
+const count = xs =>
+	xs.reduce(
+		(acc, x) => (acc[x] !== undefined ? (acc[x] += 1) : (acc[x] = 1), acc),
+		{}
+	)
 
-const last = a => a[a.length - 1]
-
-const checkMagazine = (magazine, note) => {
-	const marked = {}
-
-	for (const word of note) {
-		const i = has(word, marked[word] ? last(marked[word]) + 1 : 0)(magazine)
-
-		if (i === false) return "No"
-		else marked[word] ? marked[word].push(i) : (marked[word] = [i])
-	}
-
-	return "Yes"
+const checkMagazine = (mag, notes) => {
+	const available = count(mag)
+	for (const word of notes) if (!available[word]--) return console.log('No')
+	return console.log('Yes')
 }
